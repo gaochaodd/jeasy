45e05585c802f0cd49e2ff4e625783988ebbcafd
chao.Gao <gaochaodd@gmail.com>
2019-01-28 18:43:28 +0800

    message:添加时间加法测试 auth:chao

--- test/moment_test.js
+++ test/moment_test.js
@@ -146,6 +146,245 @@ describe('moment(value)', () => {
         expect(date.date).toBe(now.getDate());
         expect(date.day).toBe(now.getDay());
     });
+
+    it('moment()', () => {
+        date = moment();
+        let now = new Date();
+        expect(date.fullYear).toBe(now.getFullYear());
+        expect(date.month).toBe(now.getMonth() + 1);
+        expect(date.date).toBe(now.getDate());
+        expect(date.day).toBe(now.getDay());
+    });
+    
+    it('moment("1987/11/22 10:20:30").add(1, "seconds")', () => {
+        date = moment("1987/11/22 10:20:30").add(1, "seconds");
+        expect(date.fullYear).toBe(1982);
+        expect(date.month).toBe(11);
+        expect(date.date).toBe(22);
+        expect(date.hour).toBe(10);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(31);
+    });
+
+    it('moment("1987/11/22 10:20:59").add(1, "seconds")', () => {
+        date = moment("1987/11/22 10:20:59").add(1, "seconds");
+        expect(date.fullYear).toBe(1987);
+        expect(date.month).toBe(11);
+        expect(date.date).toBe(22);
+        expect(date.hour).toBe(10);
+        expect(date.minute).toBe(21);
+        expect(date.second).toBe(0);
+    });
+
+    it('moment("1987/12/31 23:59:59").add(1, "seconds")', () => {
+        date = moment("1987/12/31 23:59:59").add(1, "seconds");
+        expect(date.fullYear).toBe(1988);
+        expect(date.month).toBe(1);
+        expect(date.date).toBe(1);
+        expect(date.hour).toBe(0);
+        expect(date.minute).toBe(0);
+        expect(date.second).toBe(0);
+    });
+
+    it('moment("1987/11/22 10:20:59").add(1, minutes)', () => {
+        date = moment("1987/11/22 10:20:59").add(1, "minutes");
+        expect(date.fullYear).toBe(1987);
+        expect(date.month).toBe(11);
+        expect(date.date).toBe(22);
+        expect(date.hour).toBe(10);
+        expect(date.minute).toBe(21);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("1987/11/22 10:59:59").add(1, minutes)', () => {
+        date = moment("1987/11/22 10:20:59").add(1, "minutes");
+        expect(date.fullYear).toBe(1987);
+        expect(date.month).toBe(11);
+        expect(date.date).toBe(22);
+        expect(date.hour).toBe(11);
+        expect(date.minute).toBe(0);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("1987/11/22 10:20:59").add(1, hours)', () => {
+        date = moment("1987/11/22 10:20:59").add(1, "hours");
+        expect(date.fullYear).toBe(1987);
+        expect(date.month).toBe(11);
+        expect(date.date).toBe(22);
+        expect(date.hour).toBe(11);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("1987/11/22 23:20:59").add(1, hours)', () => {
+        date = moment("1987/11/22 23:20:59").add(1, "hours");
+        expect(date.fullYear).toBe(1987);
+        expect(date.month).toBe(11);
+        expect(date.date).toBe(23);
+        expect(date.hour).toBe(00);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("1987/11/22 23:20:59").add(1, days)', () => {
+        date = moment("1987/11/22 23:20:59").add(1, "days");
+        expect(date.fullYear).toBe(1987);
+        expect(date.month).toBe(11);
+        expect(date.date).toBe(23);
+        expect(date.hour).toBe(23);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("1987/11/30 23:20:59").add(1, days)', () => {
+        date = moment("1987/11/30 23:20:59").add(1, "days");
+        expect(date.fullYear).toBe(1987);
+        expect(date.month).toBe(12);
+        expect(date.date).toBe(1);
+        expect(date.hour).toBe(23);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("1987/07/30 23:20:59").add(1, days)', () => {
+        date = moment("1987/07/30 23:20:59").add(1, "days");
+        expect(date.fullYear).toBe(1987);
+        expect(date.month).toBe(7);
+        expect(date.date).toBe(31);
+        expect(date.hour).toBe(23);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("1987/07/31 23:20:59").add(1, days)', () => {
+        date = moment("1987/07/31 23:20:59").add(1, "days");
+        expect(date.fullYear).toBe(1987);
+        expect(date.month).toBe(8);
+        expect(date.date).toBe(1);
+        expect(date.hour).toBe(23);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("1987/11/30 23:20:59").add(1, days)', () => {
+        date = moment("1987/11/30 23:20:59").add(1, "days");
+        expect(date.fullYear).toBe(1987);
+        expect(date.month).toBe(12);
+        expect(date.date).toBe(1);
+        expect(date.hour).toBe(23);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("1987/02/28 23:20:59").add(1, days)', () => {
+        date = moment("1987/02/28 23:20:59").add(1, "days");
+        expect(date.fullYear).toBe(1987);
+        expect(date.month).toBe(3);
+        expect(date.date).toBe(1);
+        expect(date.hour).toBe(23);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("2000/02/28 23:20:59").add(1, days)', () => {
+        date = moment("2000/02/28 23:20:59").add(1, "days");
+        expect(date.fullYear).toBe(2000);
+        expect(date.month).toBe(2);
+        expect(date.date).toBe(29);
+        expect(date.hour).toBe(23);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("2000/02/28 23:20:59").add(1, days)', () => {
+        date = moment("2000/02/28 23:20:59").add(1, "days");
+        expect(date.fullYear).toBe(2000);
+        expect(date.month).toBe(2);
+        expect(date.date).toBe(29);
+        expect(date.hour).toBe(23);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("2000/02/28 23:20:59").add(1, days)', () => {
+        date = moment("2000/02/28 23:20:59").add(1, "days");
+        expect(date.fullYear).toBe(2000);
+        expect(date.month).toBe(2);
+        expect(date.date).toBe(29);
+        expect(date.hour).toBe(23);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("2000/02/28 23:20:59").add(1, days)', () => {
+        date = moment("2000/02/28 23:20:59").add(1, "days");
+        expect(date.fullYear).toBe(2000);
+        expect(date.month).toBe(2);
+        expect(date.date).toBe(29);
+        expect(date.hour).toBe(23);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("2000/01/20 23:20:59").add(1, months)', () => {
+        date = moment("2000/01/31 23:20:59").add(1, "months");
+        expect(date.fullYear).toBe(2000);
+        expect(date.month).toBe(2);
+        expect(date.date).toBe(20);
+        expect(date.hour).toBe(23);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("2000/01/31 23:20:59").add(1, months)', () => {
+        date = moment("2000/01/31 23:20:59").add(1, "months");
+        expect(date.fullYear).toBe(2000);
+        expect(date.month).toBe(2);
+        expect(date.date).toBe(29);
+        expect(date.hour).toBe(23);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("1999/01/31 23:20:59").add(1, months)', () => {
+        date = moment("2000/01/31 23:20:59").add(1, "months");
+        expect(date.fullYear).toBe(2000);
+        expect(date.month).toBe(2);
+        expect(date.date).toBe(28);
+        expect(date.hour).toBe(23);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("2000/03/31 23:20:59").add(1, months)', () => {
+        date = moment("2000/03/31 23:20:59").add(1, "months");
+        expect(date.fullYear).toBe(2000);
+        expect(date.month).toBe(4);
+        expect(date.date).toBe(30);
+        expect(date.hour).toBe(23);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("2000/03/31 23:20:59").add(1, years)', () => {
+        date = moment("2000/01/31 23:20:59").add(1, "years");
+        expect(date.fullYear).toBe(2001);
+        expect(date.month).toBe(1);
+        expect(date.date).toBe(31);
+        expect(date.hour).toBe(23);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
+
+    it('moment("2000/02/29 23:20:59").add(1, years)', () => {
+        date = moment("2000/02/29 23:20:59").add(1, "years");
+        expect(date.fullYear).toBe(2001);
+        expect(date.month).toBe(2);
+        expect(date.date).toBe(28);
+        expect(date.hour).toBe(23);
+        expect(date.minute).toBe(20);
+        expect(date.second).toBe(59);
+    });
 });
 
 describe('moment(value).format(format)', () =>  {
